5859e516b2af0b75429725be82f1e770399dad1e
516M <patatrdg@gmail.com>
2024-08-20 15:25:48 +0300

    Create div in the container with a function and loop

--- script.js
+++ script.js
@@ -2,3 +2,24 @@ let container = document.querySelector(".container");
 
 container.style.display = "flex";
 container.style.border = "1px solid black";
+
+function createAndAppendPixel(parent, index) {
+  let idx = Number.isInteger(index) ? index : "";
+  let pixel = document.createElement("div");
+
+  pixel.className = "pixel" + idx;
+  pixel.style.flex = "1 1 1em";
+  pixel.style.width = "16px";
+  pixel.style.height = "16px";
+  pixel.style.border = "1px solid black";
+  pixel.style.backgroundColor = "white";
+
+  if (parent) {
+    parent.append(pixel);
+  }
+}
+
+const GRIDS = 16;
+for (let i = 0; i < GRIDS; i++) {
+  createAndAppendPixel(container, i);
+}
