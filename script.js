a5df5737054eeebfb76cc6fda93bd7031e54914f
516M <patatrdg@gmail.com>
2024-08-22 03:10:29 +0300

    New button that creates grid based on user input

--- script.js
+++ script.js
@@ -9,12 +9,13 @@ container.style.border = "1px solid black";
 
 function createGrid(parent, size) {
   if (!parent) return;
+  if (size <= 0) return;
 
-  for (let i = 0; i < GRIDS; i++) {
+  for (let i = 0; i < size; i++) {
     let column = document.createElement("div");
     column.className = "column" + i;
 
-    for (let j = 0; j < GRIDS; j++) {
+    for (let j = 0; j < size; j++) {
       let pixel = document.createElement("div");
       pixel.className = "pixel-item" + i + "-" + j;
       pixel.style.flex = "1 1 1em";
@@ -42,3 +43,25 @@ function colorPixels(e) {
   }
 }
 container.addEventListener("mouseover", colorPixels);
+
+const btn = document.createElement("button");
+btn.textContent = "New Grid";
+btn.style.padding = "1rem";
+btn.style.margin = "1.2rem";
+
+function createGridFromClick(e) {
+  let newSize = +prompt("Enter new grid size:");
+  if (typeof newSize != "number") return;
+  if (newSize <= 0) newSize = 1;
+
+  if (newSize > 100) {
+    alert("Can't assign a grid size larger than 100!");
+  } else {
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+    createGrid(container, newSize);
+  }
+}
+btn.addEventListener("click", createGridFromClick);
+document.body.prepend(btn);
